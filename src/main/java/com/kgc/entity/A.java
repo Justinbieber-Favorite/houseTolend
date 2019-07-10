7fd86b41bb46f40f34170187b9afcfde20156235
huangwei <1425114215@qq.com>
2019-07-10 09:35:22 +0800

    A类

--- src/main/java/com/kgc/entity/A.java
+++
@@ -1,7 +0,0 @@
-package com.kgc.entity;
-
-public class A {
-    public static void main(String[] args) {
-        System.out.println("AAAAAAA");
-    }
-}
--- src/main/java/com/kgc/entity/ShaGou.java
+++
@@ -1,6 +0,0 @@
-package com.kgc.entity;
-public  class ShaGou{
-    public static void main(String[] args) {
-        System.out.println("666");
-    }
-}
